77f3e2ca013520f97ecca23755a00c26c7b52ae6
Rahul Manelkar <rahul.manelkar@gmail.com>
2025-02-15 21:09:22 -0800

    Added functionality for multiple commands terminating on 'exit' command

--- src/ccsh/main.py
+++ src/ccsh/main.py
@@ -2,8 +2,12 @@ from subprocess import run
 import sys
 
 def ccsh():
-    cmd = input("ccsh> ").strip()
-    run(cmd,stdin=sys.stdin, stdout=sys.stdout,stderr=sys.stderr)
+    while True:
+        cmd = input("ccsh> ").strip()
+        if cmd == "exit":
+            sys.exit(0)
+        run(cmd,stdin=sys.stdin, stdout=sys.stdout,stderr=sys.stderr)
+    
 
 if __name__=='__main__':
     ccsh()
